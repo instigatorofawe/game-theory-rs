626afa630d4455af76acf952267f471d2a2a5149
Ran Liu <xabsox@gmail.com>
2024-08-10 15:37:56 -0700

    Improve concision of enumeration of combos

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 pub fn factorial(x: usize) -> usize {
     (1..x + 1).product()
 }
@@ -10,25 +12,19 @@ pub fn choose(n: usize, k: usize) -> usize {
     }
 }
 
-pub fn enumerate_combos<T: Clone>(items: Vec<T>, k: usize) -> Vec<Vec<T>> {
-    // Base case: k = 1
+pub fn enumerate_combos<T: Clone + Send + Sync>(items: Vec<T>, k: usize) -> Vec<Vec<T>> {
+    // Base case: k = 1 or k == length of items
     if k == 1 {
-        return items
-            .into_iter()
-            .map(|x| {
-                let mut vec: Vec<T> = Vec::with_capacity(1);
-                vec.push(x);
-                vec
-            })
-            .collect();
+        return items.into_iter().map(|x| vec![x]).collect();
+    }
+
+    if k == items.len() {
+        return vec![items];
     }
     // For 0..n-k, choose prefix, append to enumerated combos of (n-k .. n) choose k-1
-    let prefixes = Vec::from(&items[0..items.len() - k + 1]);
-
-    // For each prefix, prefix to remainder
-    // Return result
-    let combos: Vec<Vec<T>> = prefixes
-        .into_iter()
+    // For each prefix, concatenate prefix to remainder
+    items[0..items.len() - k + 1]
+        .into_par_iter()
         .enumerate()
         .flat_map(|(i, x)| {
             let remainder = enumerate_combos(Vec::from(&items[i + 1..items.len()]), k - 1);
@@ -42,14 +38,13 @@ pub fn enumerate_combos<T: Clone>(items: Vec<T>, k: usize) -> Vec<Vec<T>> {
                 })
                 .collect::<Vec<Vec<T>>>()
         })
-        .collect();
-
-    combos
+        .collect::<Vec<Vec<T>>>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Instant;
 
     #[test]
     fn test_enumerate_combos() {
@@ -64,5 +59,27 @@ mod tests {
             3,
         );
         assert_eq!(x.len(), choose(16, 3));
+    }
+
+    #[test]
+    fn benchmark_combos() {
+        let start = Instant::now();
+        // Enumerate all combos up to n for all k up to n-1
+        let combos: Vec<usize> = (3_usize..20)
+            .into_iter()
+            .flat_map(|i| {
+                let result: Vec<usize> = (2_usize..i)
+                    .into_par_iter()
+                    .map(|j| {
+                        let combos = enumerate_combos((0..i).collect(), j);
+                        combos.len()
+                    })
+                    .collect();
+                result
+            })
+            .collect();
+
+        println!("Elapsed duration: {} ms", start.elapsed().as_millis());
+        println!("Computed {} combos", combos.iter().sum::<usize>());
     }
 }
