b70e717ab3b835a354aed9af45ab75addaf46d38
Ran Liu <xabsox@gmail.com>
2024-08-13 12:58:59 -0700

    Push Fold

--- src/push_fold.rs
+++ src/push_fold.rs
@@ -1,62 +1,263 @@
+mod cfr;
 mod utils;
 
-use rayon::prelude::*;
-use rust_poker::equity_calculator::*;
-use rust_poker::hand_range::*;
+use cfr::*;
 use utils::enumerate_combos;
 
-fn main() {
-    const CARDS: [&str; 52] = [
-        "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s", "5c", "5d", "5h",
-        "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s", "9c", "9d",
-        "9h", "9s", "Tc", "Td", "Th", "Ts", "Jc", "Jd", "Jh", "Js", "Qc", "Qd", "Qh", "Qs", "Kc",
-        "Kd", "Kh", "Ks", "Ac", "Ad", "Ah", "As",
-    ];
+use std::fmt::Display;
 
-    let choose_four: Vec<Vec<usize>> = enumerate_combos((0..52).collect::<Vec<usize>>(), 4);
-    let empty = get_card_mask("");
+use rust_poker::constants::RANK_TO_CHAR;
+use rust_poker::equity_calculator::*;
+use rust_poker::hand_range::*;
 
-    let equities: Vec<f64> = choose_four
+use ndarray::*;
+use rayon::prelude::*;
+
+struct Hand(usize, usize);
+
+impl Display for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let r1 = self.0 / 4;
+        let r2 = self.1 / 4;
+        let s1 = self.0 % 4;
+        let s2 = self.1 % 4;
+
+        if r1 == r2 {
+            write!(f, "{}{}", RANK_TO_CHAR[r1], RANK_TO_CHAR[r1])
+        } else if s1 == s2 {
+            if r1 > r2 {
+                write!(f, "{}{}s", RANK_TO_CHAR[r1], RANK_TO_CHAR[r2])
+            } else {
+                write!(f, "{}{}s", RANK_TO_CHAR[r2], RANK_TO_CHAR[r1])
+            }
+        } else {
+            if r1 > r2 {
+                write!(f, "{}{}o", RANK_TO_CHAR[r1], RANK_TO_CHAR[r2])
+            } else {
+                write!(f, "{}{}o", RANK_TO_CHAR[r2], RANK_TO_CHAR[r1])
+            }
+        }
+    }
+}
+
+impl Hand {
+    fn index_to_str(x: usize) -> String {
+        let mut result = String::with_capacity(3);
+        let i = x / 13;
+        let j = x % 13;
+
+        if i > j {
+            result.push(RANK_TO_CHAR[i]);
+            result.push(RANK_TO_CHAR[j]);
+            result.push('s');
+        } else if i < j {
+            result.push(RANK_TO_CHAR[j]);
+            result.push(RANK_TO_CHAR[i]);
+            result.push('o');
+        } else {
+            result.push(RANK_TO_CHAR[i]);
+            result.push(RANK_TO_CHAR[j]);
+        }
+
+        result
+    }
+    fn get_index(c1: usize, c2: usize) -> usize {
+        let r1 = c1 / 4;
+        let s1 = c1 % 4;
+        let r2 = c2 / 4;
+        let s2 = c2 % 4;
+
+        if (r1 == r2) || (s1 == s2) {
+            // Diagonals for pocket pairs, upper triangle for suited combos
+            13 * r1 + r2
+        } else {
+            // Lower triangle for offsuit combos
+            13 * r2 + r1
+        }
+    }
+}
+
+#[derive(Debug)]
+struct MatchupTable {
+    counts: [[u64; 169]; 169],
+}
+
+impl MatchupTable {
+    fn count_matchup(&mut self, hand_index_1: usize, hand_index_2: usize) {
+        self.counts[hand_index_1][hand_index_2] += 1;
+        self.counts[hand_index_2][hand_index_1] += 1;
+    }
+
+    fn sum(&self) -> u64 {
+        self.counts.iter().fold(0, |f, x| f + x.iter().sum::<u64>())
+    }
+}
+
+impl Default for MatchupTable {
+    fn default() -> Self {
+        MatchupTable {
+            counts: [[0; 169]; 169],
+        }
+    }
+}
+
+fn build_push_fold_tree(bbs: f64) -> Box<dyn Node> {
+    let mut matchup_table = MatchupTable::default();
+    enumerate_combos((0..52).collect::<Vec<usize>>(), 4)
+        .into_iter()
+        .map(|x| {
+            matchup_table.count_matchup(Hand::get_index(x[0], x[1]), Hand::get_index(x[2], x[3]));
+            matchup_table.count_matchup(Hand::get_index(x[0], x[2]), Hand::get_index(x[1], x[3]));
+            matchup_table.count_matchup(Hand::get_index(x[0], x[3]), Hand::get_index(x[1], x[2]));
+        })
+        .for_each(drop);
+
+    let equities: Vec<Vec<f64>> = (0_usize..169)
         .into_par_iter()
-        .flat_map(|x| {
-            let mut equities: Vec<f64> = Vec::with_capacity(6);
-
-            let mut hand_1 = String::with_capacity(4);
-            hand_1.push_str(CARDS[x[0]]);
-            hand_1.push_str(CARDS[x[1]]);
-
-            let mut hand_2 = String::with_capacity(4);
-            hand_2.push_str(CARDS[x[2]]);
-            hand_2.push_str(CARDS[x[3]]);
-
-            equities.push(
-                exact_equity(&HandRange::from_strings(vec![hand_1, hand_2]), empty, 1).unwrap()[0],
-            );
-
-            let mut hand_1 = String::with_capacity(4);
-            hand_1.push_str(CARDS[x[0]]);
-            hand_1.push_str(CARDS[x[2]]);
-
-            let mut hand_2 = String::with_capacity(4);
-            hand_2.push_str(CARDS[x[1]]);
-            hand_2.push_str(CARDS[x[3]]);
-
-            equities.push(
-                exact_equity(&HandRange::from_strings(vec![hand_1, hand_2]), empty, 1).unwrap()[0],
-            );
-
-            let mut hand_1 = String::with_capacity(4);
-            hand_1.push_str(CARDS[x[0]]);
-            hand_1.push_str(CARDS[x[3]]);
-
-            let mut hand_2 = String::with_capacity(4);
-            hand_2.push_str(CARDS[x[1]]);
-            hand_2.push_str(CARDS[x[2]]);
-
-            equities.push(
-                exact_equity(&HandRange::from_strings(vec![hand_1, hand_2]), empty, 1).unwrap()[0],
-            );
-            equities
+        .map(|i| {
+            (i..169)
+                .into_iter()
+                .map(|j| {
+                    exact_equity(
+                        &HandRange::from_strings(vec![
+                            Hand::index_to_str(i),
+                            Hand::index_to_str(j),
+                        ]),
+                        get_card_mask(""),
+                        1,
+                    )
+                    .unwrap()[0]
+                })
+                .collect::<Vec<f64>>()
         })
         .collect();
+
+    let mut equities_square: Array<f64, Ix2> = Array::zeros((169, 169));
+    equities
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| {
+            equities_square
+                .slice_mut(s![i, i..])
+                .assign(&Array::from(x.clone()));
+            equities_square
+                .slice_mut(s![i.., i])
+                .assign(&(1. - Array::from(x)));
+        })
+        .for_each(drop);
+
+    let total_matchups = matchup_table.sum();
+    let state_probabilities: Array<f64, Ix1> = matchup_table
+        .counts
+        .as_flattened()
+        .iter()
+        .map(|x| *x as f64 / total_matchups as f64)
+        .collect();
+
+    // Compute information sets
+    let infosets_p1: Vec<Vec<usize>> = (0_usize..169)
+        .map(|i| (0_usize..169).map(|j| i * 169 + j).collect::<Vec<usize>>())
+        .collect();
+    let infosets_p2: Vec<Vec<usize>> = (0_usize..169)
+        .map(|i| (0_usize..169).map(|j| j * 169 + i).collect::<Vec<usize>>())
+        .collect();
+
+    let root = Box::new(ActionNode {
+        name: "root".to_string(),
+        state_probabilities: state_probabilities,
+        total_probabilities: Array::zeros(169),
+        evs: Array::zeros(169 * 169),
+        infosets: infosets_p1,
+        strategy: Array::from_elem((2, 169), 1. / 2.),
+        avg_strategy: Array::from_elem((2, 169), 1. / 2.),
+        regrets: Array::zeros((2, 169)),
+        iter_count: 1,
+        sign: 1,
+        children: vec![
+            Box::new(ActionNode {
+                name: "b".to_string(),
+                state_probabilities: Array::zeros(169 * 169),
+                total_probabilities: Array::zeros(169),
+                evs: Array::zeros(169 * 169),
+                infosets: infosets_p2,
+                strategy: Array::from_elem((2, 169), 1. / 2.),
+                avg_strategy: Array::from_elem((2, 169), 1. / 2.),
+                regrets: Array::zeros((2, 169)),
+                iter_count: 1,
+                sign: -1,
+                children: vec![
+                    Box::new(TerminalNode {
+                        name: "bc".to_string(),
+                        state_probabilities: Array::zeros(169 * 169),
+                        payouts: Array::from_elem(169 * 169, 2. * bbs)
+                            * (0.5 - equities_square.flatten()),
+                    }),
+                    Box::new(TerminalNode {
+                        name: "bf".to_string(),
+                        state_probabilities: Array::zeros(169 * 169),
+                        payouts: Array::from_elem(169 * 169, 1.),
+                    }),
+                ],
+            }),
+            Box::new(TerminalNode {
+                name: "f".to_string(),
+                state_probabilities: Array::zeros(169 * 169),
+                payouts: Array::from_elem(169 * 169, -0.5),
+            }),
+        ],
+    });
+
+    root
+}
+
+fn main() {
+    let hand_names: Vec<String> = (0..169).map(|i| Hand::index_to_str(i)).collect();
+
+    println!("Building tree...");
+    let mut root = build_push_fold_tree(10.);
+
+    for i in 0..1000 {
+        // println!("Iteration {}", i);
+        // Run one iteration of CFR
+        root.update_probabilities();
+        root.update_ev();
+        root.update_strategy();
+    }
+
+    hand_names
+        .into_iter()
+        .zip(root.avg_strategy().unwrap().slice(s![0, ..]).into_iter())
+        .map(|(name, strategy)| {
+            println!("{}: {}", name, strategy);
+        })
+        .for_each(drop);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_index() {
+        assert_eq!(Hand::get_index(1, 0), 0);
+        assert_eq!(Hand::get_index(51, 50), 168);
+    }
+
+    #[test]
+    fn test_display() {
+        assert_eq!(Hand(1, 0).to_string(), "22");
+        assert_eq!(Hand(48, 0).to_string(), "A2s");
+    }
+
+    #[test]
+    fn test_from_index() {
+        for i in 0..51 {
+            for j in i + 1..52 {
+                assert_eq!(
+                    Hand(i, j).to_string(),
+                    Hand::index_to_str(Hand::get_index(j, i))
+                );
+            }
+        }
+    }
 }
